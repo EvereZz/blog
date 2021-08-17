e933b10459ec8a3156b179650f53f92ed8933be1
evereZz <example.com>
2021-08-17 03:02:52 +0300

    lesson 8 commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<Title>My Blog</Title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+       <?= $post; ?>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<Title>My Blog</Title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices hendrerit ante ac facilisis. In vitae convallis augue, vel dapibus purus. Curabitur vel est sagittis, vestibulum dolor at, maximus nunc. Cras posuere suscipit urna, ac vestibulum orci viverra nec. Morbi tincidunt ante dolor, ac vehicula libero luctus eget. Maecenas tincidunt enim et quam consequat, sit amet tempus elit blandit. Sed vel venenatis nisl. Nunc dapibus nibh quis faucibus vulputate. Cras vitae felis sit amet purus commodo malesuada a non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla sagittis placerat luctus. Phasellus rhoncus urna nec sem egestas molestie. Duis sit amet risus id urna laoreet rhoncus a at lorem. Aliquam sit amet nulla lorem.
+
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices hendrerit ante ac facilisis. In vitae convallis augue, vel dapibus purus. Curabitur vel est sagittis, vestibulum dolor at, maximus nunc. Cras posuere suscipit urna, ac vestibulum orci viverra nec. Morbi tincidunt ante dolor, ac vehicula libero luctus eget. Maecenas tincidunt enim et quam consequat, sit amet tempus elit blandit. Sed vel venenatis nisl. Nunc dapibus nibh quis faucibus vulputate. Cras vitae felis sit amet purus commodo malesuada a non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla sagittis placerat luctus. Phasellus rhoncus urna nec sem egestas molestie. Duis sit amet risus id urna laoreet rhoncus a at lorem. Aliquam sit amet nulla lorem.
+
+        </p>
+    </article>
+
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultrices hendrerit ante ac facilisis. In vitae convallis augue, vel dapibus purus. Curabitur vel est sagittis, vestibulum dolor at, maximus nunc. Cras posuere suscipit urna, ac vestibulum orci viverra nec. Morbi tincidunt ante dolor, ac vehicula libero luctus eget. Maecenas tincidunt enim et quam consequat, sit amet tempus elit blandit. Sed vel venenatis nisl. Nunc dapibus nibh quis faucibus vulputate. Cras vitae felis sit amet purus commodo malesuada a non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla sagittis placerat luctus. Phasellus rhoncus urna nec sem egestas molestie. Duis sit amet risus id urna laoreet rhoncus a at lorem. Aliquam sit amet nulla lorem.
+
+        </p>
+    </article>
+</body>
