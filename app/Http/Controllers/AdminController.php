bdfb505fee0adae899afda10d9ba49bf6afbfc2c
EvereZz <everezz@yahoo.com>
2021-12-01 23:32:58 +0300

    quick search fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller {
     public function index()
     {
         return view('admin.posts.index', [
-            "posts" => Post::paginate(50)
+            "posts" => Post::paginate(20)
         ]);
     }
     
